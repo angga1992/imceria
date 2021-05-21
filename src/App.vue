90dc55eab6351d21dc41112e1bbe3fb15ea27016
angga1992 <anggamulya.ghazy@gmail.com>
2021-05-21 13:24:31 +0700

    i am ceria

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,119 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <h1>Picsum Photos</h1>
+    <!-- <pre>{{data}}</pre> -->
+    <div class="user" v-for="(items, idx) in data" :key="idx">
+      <div class="user-avatar">
+        <img :src="items.download_url" />
+      </div>
+      <div class="user-details">
+        <h2 class="user-name">
+          {{ items.author }}
+        </h2>
+        <span>source:</span>
+        <a :href="items.url">{{items.url}}</a>
+      </div>
     </div>
-    <router-view/>
+    <button v-show="!mobile" @click="loadMore()">load more</button>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      data: [],
+      page: 1,
+      mobile: false
     }
+  },
+  methods: {
+    getInitialUsers () {
+      axios.get(`https://picsum.photos/v2/list?page=${this.page}&limit=10`).then((response) => {
+        this.data = response.data
+      })
+    },
+    getNextUser () {
+      window.onscroll = () => {
+        const bottomOfWindow = document.documentElement.scrollTop + window.innerHeight === document.documentElement.offsetHeight
+        if (window.innerWidth > 600) {
+          this.mobile = false
+        } else {
+          this.mobile = true
+        }
+        if (bottomOfWindow) {
+          axios.get(`https://picsum.photos/v2/list?page=${this.page}&limit=10`).then(response => {
+            for (const i in response.data) {
+              this.data.push(response.data[i])
+            }
+            this.page = this.page + 1
+          })
+        }
+      }
+    },
+    loadMore () {
+      axios.get(`https://picsum.photos/v2/list?page=${this.page}&limit=10`).then(response => {
+        for (const i in response.data) {
+          this.data.push(response.data[i])
+        }
+        this.page = this.page + 1
+      })
+    }
+  },
+  beforeMount () {
+    this.getInitialUsers()
+  },
+  mounted () {
+    this.getNextUser()
+    alert('change to mobile break point to active infinite scroll function')
   }
+}
+</script>
+
+<style lang="scss" scoped>
+#app {
+    padding: 10%;
+    background-color: antiquewhite;
+    h1 {
+        color: green;
+    }
+    .user {
+        display: flex;
+        background:white;
+        border-radius: 0.65em;
+        margin: 1em auto;
+        -webkit-box-shadow: 0 10px 6px -6px #777;
+        -moz-box-shadow: 0 10px 6px -6px #777;
+        box-shadow: 0 10px 6px -6px #777;
+        // border: 0.5px solid #777;
+        .user-avatar {
+            padding: 1em;
+            img {
+                display: block;
+                width: 64px;
+                /* min-width: 64px; */
+                height: 64px;
+                border-radius: 50%;
+            }
+        }
+        .user-details {
+            padding: 1em;
+            span {
+                padding-right: 4px;
+                font-size: 12px;
+            }
+            a {
+                font-size: 12px;
+                word-break: break-word;
+            }
+            .user-name {
+                margin: 0;
+                padding: 0;
+                font-size: 2vw;
+                font-weight: 900;
+            }
+        }
+    }
 }
 </style>
