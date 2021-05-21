b2c21ae63e632395f07905aa613307230af85665
angga1992 <anggamulya.ghazy@gmail.com>
2021-05-21 14:15:20 +0700

    dist

---
+++ dist/precache-manifest.ded687bf22534af6e37f5f7fc7a8d09d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a26089a2671ae5135116",
+    "url": "/css/app.84817a9b.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "930fb64cfdf194dfdcdd9618f36f7e40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28c0a59f6c8ca0cbd71c",
+    "url": "/js/about.41593ca5.js"
+  },
+  {
+    "revision": "a26089a2671ae5135116",
+    "url": "/js/app.fb2faa00.js"
+  },
+  {
+    "revision": "212bd3191d84d715bbfd",
+    "url": "/js/chunk-vendors.0263d492.js"
+  },
+  {
+    "revision": "230f9e3537e147df3c921547e0931862",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
